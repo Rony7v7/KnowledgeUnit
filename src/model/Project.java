571f455dce5fffc94920be34ffc80f879c635fe0
Rony Ordoñez <ronyfarido@gmail.com>
2023-03-22 16:48:04 -0500

    added attributes to class Project

--- src/model/Project.java
+++ src/model/Project.java
@@ -1,5 +1,15 @@
 package model;
 
+import java.util.*;
+
 public class Project {
     
+    private String name;
+    private String clientName;
+    private Date plannedStartDate;
+    private Date plannedEndDate;
+    private double budget;
+    
+    private String[] projectManagers;
+
 }
