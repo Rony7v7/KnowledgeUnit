deb5f9a825624ac86a4be63a511a06956db7d7e9
Rony Ordoñez <ronyfarido@gmail.com>
2023-03-22 12:07:03 -0500

    Add Menu Method

--- README.md
+++
@@ -1,2 +0,0 @@
-# KnowledgeUnit
-KnowledgeUnit
---
+++ src/ui/Main.java
@@ -0,0 +1,45 @@
+package ui;
+
+import java.util.Scanner;
+
+class Main{
+
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int option = menu();
+    }
+
+    public static int menu() {
+        int option = 0;
+
+        do{
+            System.out.print("\n--------- BIENVENIDO --------\n"+
+                               "      ELige una opción        \n\n"+
+                               "- Etapas y capsulas\n\n"+
+                               "1. Crear proyecto\n"+
+                               "2. Culminar etapa de un proyecto\n"+
+                               "3. Registrar cápsula\n"+
+                               "4. Registrar cápsula\n"+
+                               "5. Aprobar cápsula\n\n"+
+                               "- Informes y estadisticas\n\n"+
+                               "6. Listar cápsulas\n"+
+                               "7. Listar lecciones aprendidas\n"+
+                               "8. Proyecto con más capsulas registradas\n"+
+                               "9. Verificar registro de capsulas\n"+
+                               "10. Busqueda de lecciones\n\n"+
+                               " >> ");
+            option = input.nextInt();
+            input.nextLine();
+            System.out.println("");
+
+            if(option < 1 || option > 10){
+                System.out.println("Elija una opción valida *\n");
+            }
+
+        }while(option < 1 || option > 10);
+
+        return option;
+    }
+
+}
