c0a1b67c9235864d93e5b66fbdc250690de69279
Rony Ordoñez <ronyfarido@gmail.com>
2023-03-22 14:14:52 -0500

    Create executeOption method empty

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -42,4 +42,28 @@ class Main{
         return option;
     }
 
+    public static void executeOption(int option) {
+        switch (option) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+        }
+    }
 }
